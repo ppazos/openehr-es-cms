1ed21d8ecffe3dc038a510450b62ad8453d41dc3
Pablo Pazos Gutierrez <pablo.pazos@cabolabs.com>
2019-02-14 21:31:49 -0300

    initial commit for NG

---
+++ apps/cms2/config/db_config.php
@@ -0,0 +1,27 @@
+<?php
+
+$db = array(
+  'development' => array(
+    'type'     => 'mysql',
+    'url'      => 'localhost',
+    'user'     => 'root',
+    'pass'     => 'toor',
+    'database' => 'yupp_cms'
+  ),
+  'production' => array(
+    'type'     => 'mysql',
+    'url'      => 'localhost',
+    'user'     => 'root',
+    'pass'     => 'toor',
+    'database' => 'yupp_cms'
+  ),
+  'testing' => array(
+    'type'     => 'mysql',
+    'url'      => 'localhost',
+    'user'     => 'root',
+    'pass'     => 'toor',
+    'database' => 'yupp_cms_test'
+  )
+);
+
+?>
